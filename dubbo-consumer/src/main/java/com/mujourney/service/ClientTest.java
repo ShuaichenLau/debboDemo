55247d311f11844a1d78e66a3ea54fddb3c09e0c
68754807@qq.com <liujian68754807>
2019-04-20 17:26:07 +0800

    debbo zookeeper Demo

---
+++ dubbo-consumer/src/main/java/com/mujourney/service/ClientTest.java
@@ -0,0 +1,22 @@
+package com.mujourney.service;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Component
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/application-consumer.xml"})
+public class ClientTest {
+	
+	@Reference(version="1.0.0")
+	private UserService uservice;
+	
+	@Test
+	public void pors()  {
+		System.out.println(uservice.findById(1));
+	}
+}
